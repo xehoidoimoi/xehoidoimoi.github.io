11c5adc871da6e842007f4d741079d2fa85dc31e
xehoidoimoi <74006272+xehoidoimoi@users.noreply.github.com>
2020-11-06 22:31:25 +0700

    bai 1

---
+++ lesson_1/main.js
@@ -0,0 +1,25 @@
+// var myString = {
+//       age: 26,
+//       name: 'Nha Nguyen',
+//       address: 'Tp .HCM',
+// }
+//       console.log(myString['age']);
+      
+
+// for (var key in myString) {
+//       console.log(myString[key]);
+// }
+
+var myInfo = [
+      'Nguyên',
+      'Tiên',
+      'Thủy',
+
+]
+
+for (var key in myInfo)
+console.log(key)
+
+// var language = 'Javascript'
+// for (var key in language)
+// console.log(language[key])
